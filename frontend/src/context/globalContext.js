bad846db888c481427551f6b74fa6904ffc3272c
suraj paul <paulsuraj044@gmail.com>
2025-05-06 20:54:23 +0530

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5500/api/v1/";
+const BASE_URL = "https://expense-tracker-api-ot7c.onrender.com/api/v1/";
 
 const GlobalContext = React.createContext();
 
